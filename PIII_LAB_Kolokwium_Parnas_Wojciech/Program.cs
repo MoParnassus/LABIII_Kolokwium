f3ae08703e8cc2d2e8a4068800c6ae5c30fd8e25
MoParnassus <56222484+MoParnassus@users.noreply.github.com>
2019-11-10 16:47:44 +0100

    Signed-off-by: MoParnassus <56222484+MoParnassus@users.noreply.github.com>

---
+++ PIII_LAB_Kolokwium_Parnas_Wojciech/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace PIII_LAB_Kolokwium_Parnas_Wojciech
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            
+            int x = 2;
+            int y = 0;
+            y = 2 * x ^ 3 - 8 * x ^ 2 - 3 * x + 10;
+
+            Console.WriteLine("Podaj x =");
+            Console.ReadLine();
+            Console.Write("y =");
+            Console.WriteLine(y);
+        }
+        
+    }
+
+}
